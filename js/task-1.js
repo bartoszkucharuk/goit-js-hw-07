582d03950970af1b7eac6625840e80de6975cffc
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-06-02 19:35:16 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -3,6 +3,6 @@ const categoriesItems = categories.children;
 console.log(`Number of categories: ${categoriesItems.length}`);
 
  for (const listItems of categoriesItems) {
-    console.log(`Category: ${listItems.querySelector('h2').textContent}`);
-    console.log(`Elements: ${listItems.querySelectorAll('li').length}`)
+    console.log(`Category name: ${listItems.querySelector('h2').textContent}`);
+    console.log(`Numbers of elements: ${listItems.querySelectorAll('li').length}`)
 }
