e5e4b11fdb9193784dbef35624a2d0a9024c4edf
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-06-07 17:33:44 +0200

    Update task-5.js

--- js/task-5.js
+++ js/task-5.js
@@ -7,9 +7,8 @@ const changeColorBtn = document.querySelector(".change-color");
 const colorSpan = document.querySelector(".color");
 
 changeColorBtn.addEventListener("click", function () {
-  const randomColor = getRandomHexColor();
-
-  document.body.style.backgroundColor = randomColor;
-
-  colorSpan.textContent = randomColor;
+    const randomColor = getRandomHexColor();
+    
+    document.body.style.backgroundColor = randomColor;
+    colorSpan.textContent = randomColor;
 });
