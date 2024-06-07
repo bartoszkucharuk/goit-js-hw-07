ba2706c17a20aa990359db8ed5e483b8eff6db67
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-06-07 13:25:11 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -2,14 +2,13 @@ const form = document.querySelector(".login-form");
 
 form.addEventListener("submit", event => {
     event.preventDefault();
-    const formEmail = form.elements.email.value.trim();
-    const formPassword = form.elements.password.value.trim();
+    const email = form.elements.email.value.trim();
+    const password = form.elements.password.value.trim();
 
-    if (!formEmail || !formPassword) {
+    if (!email || !password) {
         alert(`All form fields must be filled in`);
     } else {
-        const formData = { formEmail, formPassword };
-
+        const formData = {email, password};
         console.log(formData);
         form.reset();
     }
