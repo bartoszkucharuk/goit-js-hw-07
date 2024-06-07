9383e1eb539b61e80bf2d7893e82785e9a1643d7
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-06-07 11:15:51 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -9,3 +9,21 @@
   </label>
   <button type="submit">Log in</button>
 </form>
+
+const registerForm = document.querySelector(".form");
+
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const login = form.elements.login.value;
+  const password = form.elements.password.value;
+  
+  if (login === "" || password === "") {
+    return console.log("Please fill in all the fields!");
+  }
+
+  console.log(`Login: ${login}, Password: ${password}`);
+  form.reset();
+}
