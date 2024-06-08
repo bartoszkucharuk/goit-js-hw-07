2c9ab0b01aa2d1b736aba6d96ce1db55125d88cd
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-06-08 10:11:19 +0200

    Update task-6.js

---
+++ js/task-6.js
@@ -0,0 +1,33 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const input = document.querySelector(`input[type="number"]`);
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesContainer = document.getElementById("boxes");
+
+// adding new boxes function by creating whole new fragment of document in memory 
+function createBoxes(amount) {
+    const dFrag = document.createDocumentFragment();
+    let size = 30;
+
+    for (let i = 0; i < amount; i++) {
+        const div = document.createElement(`div`);
+        div.style.width = `${size}`;
+        div.style.height = `${size}`;
+        div.style.background = getRandomHexColor();
+        dFrag.appendChild(div);
+        size += 10;
+    }
+    // clearing content by insert empty space in code
+    boxesContainer.innerHtml = ``;
+    boxesContainer.appendChild(dFrag);
+}
+    
+function destroyBoxes() {
+        boxesContainer.innerHTML = ``;
+}
+
