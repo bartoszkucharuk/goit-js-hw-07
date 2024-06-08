8ea0855076d234fbd2842f21c9fd23dd91ce7899
bartoszkucharuk <95539507+bartoszkucharuk@users.noreply.github.com>
2024-06-08 10:14:43 +0200

    Update task-6.js

--- js/task-6.js
+++ js/task-6.js
@@ -1,37 +1,38 @@
+const input = document.querySelector('input[type="number"]');
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", handleCreate);
+destroyBtn.addEventListener("click", handleDestroy);
+
+function handleCreate() {
+  const amount = parseInt(input.value);
+
+  if (isNaN(amount) || amount < 1 || amount > 100) {
+    return;
+  }
+
+  createBoxes(amount);
+  input.value = "";
+}
+
+function createBoxes(amount) {
+  let size = 30;
+  let innerHTML = "";
+  for (let i = 0; i < amount; i++) {
+    innerHTML += `<div style="width: ${size}px; height: ${size}px; background-color: ${getRandomHexColor()};"></div>`;
+    size += 10;
+  }
+  boxesContainer.innerHTML = innerHTML;
+}
+
+function handleDestroy() {
+  boxesContainer.innerHTML = "";
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, "0")}`;
 }
-const controls = document.getElementById(`controls`);
-const input = document.querySelector(`input`);
-const createBtn = controls.querySelector(`[data-create]`);
-const destroyBtn = controls.querySelector(`[data-destroy]`);
-const boxesContainer = document.getElementById(`boxes`);
-function createBoxes(amount) {
-  const fragment = document.createDocumentFragment();
-  let size = 30;
-  for (let i = 0; i<amount; i++) {
-    const div = document.createElement(`div`);
-    div.style.width = `${size}px`;
-    div.style.height = `${size}px`;
-    div.style.backgroundColor = getRandomHexColor();
-    fragment.appendChild(div);
-    size +=10;
-  }
-boxesContainer.innerHTML = ``;
-boxesContainer.appendChild(fragment);}
- function destroyBoxes() {
-  boxesContainer.innerHTML = ``;
- }
-createBtn/addEventListener('click', () => {
-  const amount = Number(input.value);
-  if (amount >= 1 && amount <= 100){
-    createBoxes(amount);
-    input.value = ``;
-  }
-});
-destroyBtn.addEventListener(`click`, () => {
-  destroyBoxes();
-
-})
